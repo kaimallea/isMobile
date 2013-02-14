488c3184c26cc767c6aa635ae140bfbfda7bd0a8
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-02-14 07:58:49 -0800

    Add unit test for android tablet useragent

--- tests/spec/isMobileSpec.js
+++ tests/spec/isMobileSpec.js
@@ -30,6 +30,27 @@ describe("isMobile", function() {
 
     });
 
+    describe("Tablet UserAgent", function() {
+
+      beforeEach(function() {
+        userAgent = "Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev>(KHTML, like Gecko) Chrome/<Chrome Rev> Safari/<WebKit Rev>";
+        mobile = new isMobile.Class(userAgent);
+      });
+
+      it("should not be an Android Phone", function() {
+        expect(mobile.android.phone).not.toBe(true);
+      });
+
+      it("should be an Android Tablet", function() {
+        expect(mobile.android.tablet).toBe(true);
+      });
+
+      it("should be an Android device", function() {
+        expect(mobile.android.device).toBe(true);
+      });
+
+    });
+
   });
 
 
