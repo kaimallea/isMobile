3f553d296452f046c087f37c3ce95c1a1af02b59
Igor Ribeiro Lima <lima.igorribeiro@gmail.com>
2013-02-14 08:20:09 -0800

    Add unit test for Windows Phone devices

--- tests/spec/OtherSpec.js
+++ tests/spec/OtherSpec.js
@@ -32,4 +32,29 @@ describe("Other Mobile Devices", function(){
 
   });
 
+  describe("Windows Phone", function() {
+
+    beforeEach(function() {
+      userAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows Phone OS 7.5; Trident/5.0; IEMobile/9.0)";
+      mobile = new isMobile.Class(userAgent);
+    });
+
+    it("should be a Windows Phone device", function() {
+      expect(mobile.other.windows).toBe(true);
+    });
+
+    it("should not be an Android device", function() {
+      expect(mobile.android.device).not.toBe(true);
+    });
+
+    it("should not be an Apple device", function() {
+      expect(mobile.apple.device).not.toBe(true);
+    });
+
+    it("should be a mobile device", function() {
+      expect(mobile.any).toBe(true);
+    });
+
+  });
+
 });
