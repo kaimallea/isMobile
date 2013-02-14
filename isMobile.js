e7999e0f2a67d94a3a447fad0585f72af2662c10
Igor Lima <igor.lima@evolut.io>
2013-02-14 21:26:16 -0200

    Detect firefox OS mobile

--- isMobile.js
+++ isMobile.js
@@ -18,7 +18,8 @@
         android_tablet   = /Android/i,
         other_blackberry = /BlackBerry/i,
         other_opera      = /Opera Mini/i,
-        other_windows    = /IEMobile/i
+        other_windows    = /IEMobile/i,
+        other_firefox    = /(?=.*\bFirefox\b)(?=.*\bMobile\b)/i, // Match 'Firefox' AND 'Mobile'
         seven_inch = new RegExp(
             '(?:' +         // Non-capturing group
 
@@ -66,7 +67,8 @@
             blackberry: match(other_blackberry, ua),
             opera:      match(other_opera, ua),
             windows:    match(other_windows, ua),
-            device:     match(other_blackberry, ua) || match(other_opera, ua) || match(other_windows, ua)
+            firefox:    match(other_firefox, ua),
+            device:     match(other_blackberry, ua) || match(other_opera, ua) || match(other_windows, ua) || match(other_firefox, ua)
         };
         this.seven_inch = match(seven_inch, ua);
         this.any = this.apple.device || this.android.device || this.other.device || this.seven_inch;
