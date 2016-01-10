396777c7936490c06d2f01515e7ee54683d4d484
softwarespot <softwarespotapps@gmail.com>
2016-01-10 11:34:45 +0200

    Minor changes, including single var declarations

--- isMobile.js
+++ isMobile.js
@@ -9,12 +9,9 @@
  *
  * @license: http://creativecommons.org/publicdomain/zero/1.0/
  */
-(function isMobileModule(root, factory) {
+(function isMobileModule(root, name, factory) {
     // UMD (Universal Module Definition)
     // URL: https://github.com/umdjs/umd
-
-    // Constants
-    var MODULE_NAME = 'isMobile';
 
     // Initiate the factory for the purposes of creating a class module
     var isMobileFactory = factory(root);
@@ -33,52 +30,52 @@
         module.exports = instantiate();
     } else if (typeof define === 'function' && define.amd) {
         // AMD
-        define(MODULE_NAME, [], root[MODULE_NAME] = instantiate());
+        define(name, [], root[name] = instantiate());
     } else {
         // root e.g. window
-        root[MODULE_NAME] = instantiate();
+        root[name] = instantiate();
     }
-})(this, function isMobileInterface(global) {
+})(this, 'isMobile', function isMobileInterface(global) {
     // Define the module interface
 
-     var reApplePhone        = /iPhone/i,
-        reAppleIpod          = /iPod/i,
-        reAppleTablet        = /iPad/i,
-        reAndroidPhone       = /(?=.*\bAndroid\b)(?=.*\bMobile\b)/i, // Match 'Android' AND 'Mobile'
-        reAndroidTablet      = /Android/i,
-        reAmazonPhone        = /(?=.*\bAndroid\b)(?=.*\bSD4930UR\b)/i,
-        reAmazonTablet       = /(?=.*\bAndroid\b)(?=.*\b(?:KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i,
-        reWindowsPhone       = /IEMobile/i,
-        reWindowsTablet      = /(?=.*\bWindows\b)(?=.*\bARM\b)/i, // Match 'Windows' AND 'ARM'
-        reOtherBlackberry    = /BlackBerry/i,
-        reOtherBlackberry_10 = /BB10/i,
-        reOtherOpera         = /Opera Mini/i,
-        reOtherChrome        = /(CriOS|Chrome)(?=.*\bMobile\b)/i,
-        reOtherFirefox       = /(?=.*\bFirefox\b)(?=.*\bMobile\b)/i, // Match 'Firefox' AND 'Mobile'
-        reSevenInch = new global.RegExp(
-            '(?:' +         // Non-capturing group
+    var reApplePhone = /iPhone/i;
+    var reAppleIpod = /iPod/i;
+    var reAppleTablet = /iPad/i;
+    var reAndroidPhone = /(?=.*\bAndroid\b)(?=.*\bMobile\b)/i; // Match 'Android' AND 'Mobile'
+    var reAndroidTablet = /Android/i;
+    var reAmazonPhone = /(?=.*\bAndroid\b)(?=.*\bSD4930UR\b)/i;
+    var reAmazonTablet = /(?=.*\bAndroid\b)(?=.*\b(?:KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i;
+    var reWindowsPhone = /IEMobile/i;
+    var reWindowsTablet = /(?=.*\bWindows\b)(?=.*\bARM\b)/i; // Match 'Windows' AND 'ARM'
+    var reOtherBlackberry = /BlackBerry/i;
+    var reOtherBlackberry_10 = /BB10/i;
+    var reOtherOpera = /Opera Mini/i;
+    var reOtherChrome = /(CriOS|Chrome)(?=.*\bMobile\b)/i;
+    var reOtherFirefox = /(?=.*\bFirefox\b)(?=.*\bMobile\b)/i; // Match 'Firefox' AND 'Mobile'
+    var reSevenInch = new global.RegExp(
+        '(?:' + // Non-capturing group
 
-            'Nexus 7' +     // Nexus 7
+        'Nexus 7' + // Nexus 7
 
-            '|' +           // OR
+        '|' + // OR
 
-            'BNTV250' +     // B&N Nook Tablet 7 inch
+        'BNTV250' + // B&N Nook Tablet 7 inch
 
-            '|' +           // OR
+        '|' + // OR
 
-            'Kindle Fire' + // Kindle Fire
+        'Kindle Fire' + // Kindle Fire
 
-            '|' +           // OR
+        '|' + // OR
 
-            'Silk' +        // Kindle Fire, Silk Accelerated
+        'Silk' + // Kindle Fire, Silk Accelerated
 
-            '|' +           // OR
+        '|' + // OR
 
-            'GT-P1000' +    // Galaxy Tab 7 inch
+        'GT-P1000' + // Galaxy Tab 7 inch
 
-            ')',            // End non-capturing group
+        ')', // End non-capturing group
 
-            'i');           // Case-insensitive matching
+        'i'); // Case-insensitive matching
 
     // Match a regular expression with a user agent string.
     function match(regex, userAgent) {
@@ -96,37 +93,37 @@
         }
 
         this.apple = {
-            phone:  match(reApplePhone, ua),
-            ipod:   match(reAppleIpod, ua),
+            phone: match(reApplePhone, ua),
+            ipod: match(reAppleIpod, ua),
             tablet: !match(reApplePhone, ua) && match(reAppleTablet, ua),
             device: match(reApplePhone, ua) || match(reAppleIpod, ua) || match(reAppleTablet, ua)
         };
 
         this.amazon = {
-            phone:  match(reAmazonPhone, ua),
+            phone: match(reAmazonPhone, ua),
             tablet: !match(reAmazonPhone, ua) && match(reAmazonTablet, ua),
             device: match(reAmazonPhone, ua) || match(reAmazonTablet, ua)
         };
 
         this.android = {
-            phone:  match(reAmazonPhone, ua) || match(reAndroidPhone, ua),
+            phone: match(reAmazonPhone, ua) || match(reAndroidPhone, ua),
             tablet: !match(reAmazonPhone, ua) && !match(reAndroidPhone, ua) && (match(reAmazonTablet, ua) || match(reAndroidTablet, ua)),
             device: match(reAmazonPhone, ua) || match(reAmazonTablet, ua) || match(reAndroidPhone, ua) || match(reAndroidTablet, ua)
         };
 
         this.windows = {
-            phone:  match(reWindowsPhone, ua),
+            phone: match(reWindowsPhone, ua),
             tablet: match(reWindowsTablet, ua),
             device: match(reWindowsPhone, ua) || match(reWindowsTablet, ua)
         };
 
         this.other = {
-            blackberry:   match(reOtherBlackberry, ua),
+            blackberry: match(reOtherBlackberry, ua),
             blackberry10: match(reOtherBlackberry_10, ua),
-            opera:        match(reOtherOpera, ua),
-            firefox:      match(reOtherFirefox, ua),
-            chrome:       match(reOtherChrome, ua),
-            device:       match(reOtherBlackberry, ua) || match(reOtherBlackberry_10, ua) || match(reOtherOpera, ua) || match(reOtherFirefox, ua) || match(reOtherChrome, ua)
+            opera: match(reOtherOpera, ua),
+            firefox: match(reOtherFirefox, ua),
+            chrome: match(reOtherChrome, ua),
+            device: match(reOtherBlackberry, ua) || match(reOtherBlackberry_10, ua) || match(reOtherOpera, ua) || match(reOtherFirefox, ua) || match(reOtherChrome, ua)
         };
 
         this.seven_inch = match(reSevenInch, ua);
@@ -138,8 +135,6 @@
         // Excludes 7 inch devices, classifying as phone or tablet is left to the user
         this.tablet = this.apple.tablet || this.android.tablet || this.windows.tablet;
 
-        if (global === undefined) {
-            return this;
-        }
+        return global === undefined ? this : global;
     };
 });
