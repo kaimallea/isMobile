7abf2492fc0705f69f5d93109ab56360062e44a7
softwarespot <softwarespotapps@gmail.com>
2016-01-31 00:23:49 +0200

    Tidied tests and grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,38 @@
-module.exports = function(grunt) {
-  'use strict';
+module.exports = function (grunt) {
+    'use strict';
 
-  grunt.initConfig({
-    jasmine: {
-      options: {
-        specs:   'tests/spec/*.js'
-      },
-      isMobile: {
-        src: ['isMobile.js']
-      },
-      isMobileMin: {
-        src: ['isMobile.min.js']
-      }
-    },
+    grunt.initConfig({
+        jasmine: {
+            options: {
+                specs: 'tests/spec/*.js',
+            },
+            isMobile: {
+                src: ['isMobile.js'],
+            },
+            isMobileMin: {
+                src: ['isMobile.min.js'],
+            },
+        },
 
-    uglify: {
-      options: {
-        report: 'min'
-      },
-      main: {
-        files: {
-          'isMobile.min.js': ['isMobile.js']
-        }
-      }
-    },
+        uglify: {
+            options: {
+                report: 'min',
+            },
+            main: {
+                files: {
+                    'isMobile.min.js': ['isMobile.js'],
+                },
+            },
+        },
 
-    jshint: {
-      all: ['Gruntfile.js', 'isMobile.js', 'tests/spec/**/*.js']
-    }
+        jshint: {
+            all: ['Gruntfile.js', 'isMobile.js', 'tests/spec/**/*.js'],
+        },
+    });
 
-  });
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
-
+    grunt.registerTask('default', ['jshint', 'uglify', 'jasmine']);
 };
